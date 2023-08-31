33186cbd008fb9dee717dba607bd2928d94a0847
kalwhyte <superkalel55@gmail.com>
2023-08-31 22:47:01 +0100

    log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,3 +14,36 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records using filter_datum"""
+        return filter_datum(RedactingFormatter.FIELDS,
+                            RedactingFormatter.REDACTION,
+                            super(RedactingFormatter,
+                                  self).format(record),
+                            RedactingFormatter.SEPARATOR)
+
+    FIELDS = ['name', 'email', 'phone', 'ssn', 'password', 'ip']
+
+
+def get_logger() -> logging.Logger:
+    """returns a logging.Logger object"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter())
+    logger.addHandler(handler)
+    return logger
