be77f1b6e7068de01228bc5054c8495e85c3e162
Ejiofor Emmanuel Arinze <superkalel55@gmail.com>
2023-09-01 00:45:05 +0100

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,28 +18,20 @@ def filter_datum(fields: List[str], redaction: str,
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT_FIELDS = ["name", "levelname", "asctime", "message"]
     SEPARATOR = ";"
 
     def __init__(self, fields=()):
-        """init method"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-    def filter_datum(self, fields: List[str], redaction: str,
-                     message: str, separator: str) -> str:
-        """returns the log message obfuscated"""
-        if fields in self.fields:
-            return self.REDACTION
-        return message
-
     def format(self, record: logging.LogRecord) -> str:
-        """filter values in incoming log records using filter_datum"""
-        data = super().format(record)
-        for field in self.fields:
-            data = data.replace(
-                f"{field}=", f"{field}={self.REDACTION}{self.SEPARATOR}")
-        return data
+        """ filter values in incoming log records using filter_datum.
+        """
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return txt
