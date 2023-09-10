83d041668ef04e33839e58a990bb99a67b9c79bf
Whytewalker <whytewalkerinstallation@gmail.com>
2023-09-10 08:04:12 +0100

    api

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,9 +9,6 @@ from flask_cors import (CORS, cross_origin)
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_db_auth import SessionDBAuth
-from api.v1.auth.session_exp_auth import SessionExpAuth
 
 
 app = Flask(__name__)
@@ -23,12 +20,12 @@ if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
     auth = BasicAuth()
-if auth_type == 'session_auth':
+'''if auth_type == 'session_auth':
     auth = SessionAuth()
 if auth_type == 'session_exp_auth':
     auth = SessionExpAuth()
 if auth_type == 'session_db_auth':
-    auth = SessionDBAuth()
+    auth = SessionDBAuth()'''
 
 
 @app.errorhandler(404)
@@ -64,13 +61,12 @@ def authenticate_user():
             "/api/v1/auth_session/login/",
         ]
         if auth.require_auth(request.path, excluded_paths):
-            user = auth.current_user(request)
+            request.current_user = auth.current_user(request)
             if auth.authorization_header(request) is None and \
                     auth.session_cookie(request) is None:
                 abort(401)
-            if user is None:
+            if auth.current_user(request) is None:
                 abort(403)
-            request.current_user = user
 
 
 if __name__ == "__main__":
