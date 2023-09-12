d3eb0c8d8d8c2dc7f0a1b54f446eecc0ede1a2aa
kalwhyte <superkalel55@gmail.com>
2023-09-12 14:20:59 +0100

    Update

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+""" database module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound, InvalidRequestError
+
+from user import Base, User
+
+
+class DB:
+    """ DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ Add a user instance to the session DB
+
+        Args:
+            email (str): email of the user
+            hashed_password (str): hashed password of the user.
+
+        Returns:
+            User: the user instance
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds a user instance in the DB
+
+        Args:
+            kwargs (dict): arguments to search for a user
+
+        Warning:
+            NoResultFound and InvalidRequestError exceptions are raised when no results are found
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
