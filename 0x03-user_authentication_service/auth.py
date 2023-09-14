fa4372efa989c8529203309f6beb2df19df055b2
kalwhyte <superkalel55@gmail.com>
2023-09-14 12:59:29 +0100

    Update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -26,3 +27,28 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError('User {} already exists'.format(email))
+    
+    def valid_login(self, email: str, password: str) -> bool:
+        '''valid login
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), user.hashed_password)
+        except NoResultFound:
+            return False
+        
+    def _generate_uuid(self) -> str:
+        '''generate uuid
+        '''
+        return str(uuid.uuid4())
+    
+    def create_session(self, email: str) -> str:
+        '''create session
+        '''
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = self._generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
