db1056fa7a6c89505629345a85d540d582885dbb
kalwhyte <superkalel55@gmail.com>
2023-09-14 12:43:50 +0100

    Update

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" set up a basic Flask app
+"""
+from flask import Flask, jsonify, request
+from auth import Auth
+from typing import TypeVar
+
+
+app = Flask(__name__)
+AUTH = Auth()
+T = TypeVar('T')
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """ GET /
+    Return:
+      - welcome message
+    """
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
